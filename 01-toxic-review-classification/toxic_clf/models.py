fe1579b13fa9b359c0412d789a57d483ce6ee556
Anastasia Belova <abelova@astralinux.ru>
2024-10-19 19:55:49 +0300

    add prints

--- 01-toxic-review-classification/toxic_clf/models.py
+++ 01-toxic-review-classification/toxic_clf/models.py
@@ -22,6 +22,8 @@ def classifier(dataset, model):
     skf = StratifiedKFold(n_splits=10, random_state=42, shuffle=True)
     scores = []
     for train_index, test_index in tqdm(skf.split(X, y), desc="Cross-Validating"):
+        print(train_index, type(train_index))
+        print(X)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         real_model.fit(X_train, y_train)
