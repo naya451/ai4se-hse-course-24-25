e750880e6f9293e9252909a2df75e625cd1d3bf1
Anastasia Belova <abelova@astralinux.ru>
2024-10-19 15:14:15 +0300

    implement cleaning data

--- 01-toxic-review-classification/toxic_clf/data.py
+++ 01-toxic-review-classification/toxic_clf/data.py
@@ -4,9 +4,27 @@ import datasets
 import pandas as pd
 
 
+# Функция для очистки текста
+def clean_text(text):
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    
+    contractions = {
+        "doesn’t": "does not",
+        "we’re": "we are"
+    }
+    for contraction, full_form in contractions.items():
+        text = text.replace(contraction, full_form)
+
+    text = re.sub(r'(.)\1+', r'\1', text) 
+    text = re.sub(r'[&^#*]', '', text)
+
+    return text
+
 def prepare(raw_data: Path) -> datasets.Dataset:
     dataset = pd.read_excel(raw_data)
-    # Implement dataset preparation code here
+    dataset.dropna(inplace=True) 
+    dataset.drop_duplicates(inplace=True) 
+    data['cleaned_text'] = data['message'].apply(clean_text)
     return datasets.Dataset.from_dict({'fake_data': [[1, 2], [3, 4]]})
 
 
